43da2a8fe13e1d6269816085aa6f01c891120a6a
jvannybrss <jvanny78@gmail.com>
2026-06-24 17:05:30 +0700

    Menambahkan validasi form dan placeholder

--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
 const form = document.getElementById("dataForm");
 
-if(form){
+if (form) {
 
-    form.addEventListener("submit", function(e){
+    form.addEventListener("submit", function (e) {
 
         e.preventDefault();
+        const nama = document.getElementById("nama").value.trim();
+const idPekerja = document.getElementById("nim").value.trim();
+const layanan = document.getElementById("layanan").value;
+
+if(nama === ""){
+    alert("Nama lengkap harus diisi!");
+    return;
+}
+
+if(!/^\d{6}$/.test(idPekerja)){
+    alert("ID Pekerja harus terdiri dari tepat 6 angka!");
+    return;
+}
+
+if(layanan === ""){
+    alert("Silakan pilih jenis layanan terlebih dahulu!");
+    return;
+}
 
         const data = {
             nama: document.getElementById("nama").value,
@@ -14,7 +32,7 @@ if(form){
         };
 
         let daftarData =
-        JSON.parse(localStorage.getItem("dataPekerja")) || [];
+            JSON.parse(localStorage.getItem("dataPekerja")) || [];
 
         daftarData.push(data);
 
@@ -31,10 +49,10 @@ if(form){
 
 const table = document.getElementById("dataTable");
 
-if(table){
+if (table) {
 
     let daftarData =
-    JSON.parse(localStorage.getItem("dataPekerja")) || [];
+        JSON.parse(localStorage.getItem("dataPekerja")) || [];
 
     daftarData.forEach(item => {
 
